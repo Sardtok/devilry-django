bc1782b71165a66a8730aedde29d2e16ca004dc9
Sigmund Hansen <sigmunha@student.uio.no>
2011-07-28 13:28:14 +0200

    Cleaned up the main view a lot. Should be more responsive now.

--- devilry/projects/trix/apps/trix/views.py
+++ devilry/projects/trix/apps/trix/views.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from devilry.apps.core.models import Period
 from models.status import Status
 from models.exercisestatus import ExerciseStatus
+from models.periodexercise import PeriodExercise
 
 
 def get_level(points=0):
@@ -35,41 +38,24 @@ def get_points(user):
     return points_total
 
 def main(request):
-    periods = [p for p in list(Period.objects.all()) if p.is_active()]
+    all_exercises = PeriodExercise.objects.filter(period__start_time__lte
+                                                  =datetime.now(),
+                                                  period__end_time__gte
+                                                  =datetime.now())
     exercises = {}
-    exc_stats = {}
-    student_periods = []
-    points_total = 0
+    for exercise in all_exercises:
+        e = {'title': exercise.exercise.long_name,
+             'text': exercise.exercise.text,
+             'status': -1}
 
-    for period in periods:
-
-        try:
-            if period.relatedstudents.get(user__id=request.user.id):
-                student_periods.append(period.id)
-                print "Student in period:", period.short_name
-        except:
-            print "Not a student in period:", period.short_name
-            pass
-
-        for student in period.relatedstudents.all():
-            print student.user.username
-
-        p_excs = period.exercises.all()
-        if p_excs:
-            add = {}
-            for exc in p_excs:
-                e = {'title': exc.exercise.long_name,
-                     'text': exc.exercise.text, 'status': -1}
-
-                if request.user.is_authenticated():
-                    try:
-                        stats = exc.student_results.get(student=request.user)
-                        points_total += stats.status.percentage * exc.points
-                        e.update([['status', stats.status.id]]);
-                    except ExerciseStatus.DoesNotExist, exception:
-                        pass
-                add.update( [[exc.id, e]] )
-            exercises.update([[period, add]])
+        if request.user.is_authenticated():
+            try:
+                stats = exercise.student_results.get(student=request.user)
+                e.update([['status', stats.status.id]])
+            except ExerciseStatus.DoesNotExist:
+                pass
+            exercises.setdefault(exercise.period, {}).update([[exercise.id,
+                                                                  e]])
 
     statuses = []
     if request.user.is_authenticated():
@@ -77,11 +63,8 @@ def main(request):
 
     return render(request,'trix/main.django.html',
                   {'exercises': exercises,
-                   'exc_stats': exc_stats,
                    'statuses': statuses,
-                   'student_periods': student_periods,
-                   'points': points_total,
-                   'level_info': get_level(int(points_total))})
+                   'level_info': get_level(get_points(request.user))})
 #                  {'exercises': Period.objects.all().exercises.all()})
 
 def profile(request):
