f1558ee0997519fd58341e157ddbb19a88abb318
Erlend Larsen Vestad <erlendlv@maglor.ifi.uio.no>
2011-09-04 17:36:29 +0200

    Implemented forms to create a new user and added some translations for the authentication app

--- devilry/apps/authenticate/urls.py
+++ devilry/apps/authenticate/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('devilry.apps.authenticate',
                        url(r'^logout$', 'views.logout', name='logout'),
-                       url(r'^login$', 'views.login', name='login'))
+                       url(r'^login$', 'views.login', name='login'),
+                       url(r'^register$', 'views.register', name='register'))
