da3963fd2d3e8368e2328543495b22a4dde1d8ee
Sigmund Hansen <sigmunha@student.uio.no>
2012-06-19 13:27:11 +0200

    Updated the login page to redirect if the user is already logged in. Updated the logout page to redirect to the index instead of the login page, the index is specific for Trix and this helps hide the unused Devilry pages.

--- devilry/apps/authenticate/views.py
+++ devilry/apps/authenticate/views.py
@@ -1,26 +1,22 @@
 from django.contrib import auth
-from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django import http
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.shortcuts import render
-from django.utils.translation import gettext_lazy as _
 
 
 def logout(request):
     auth.logout(request)
-    return http.HttpResponseRedirect(reverse('login'))
+    return http.HttpResponseRedirect(reverse('trix'))
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label=_("Username"))
+    username = forms.CharField()
     next = forms.CharField(widget=forms.HiddenInput,
             required=False)
-    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
 
 def login(request):
     login_failed = False
@@ -36,39 +32,14 @@ def login(request):
                             settings.DEVILRY_URLPATH_PREFIX or '/'
                     return http.HttpResponseRedirect(next)
                 else:
-                    return http.HttpResponseForbidden("Account is not active")
+                    return http.HttpResponseForbidden("Acount is not active")
             else:
                 login_failed = True
+    elif request.user.is_authenticated():
+        return http.HttpResponseRedirect(next)
     else:
         form = LoginForm(initial={'next': request.GET.get('next')})
     return render(request,
                   'authenticate/login.django.html',
                   {'form': form,
-                   'login_failed': login_failed,
-                   'register': False})
-
-def register(request):
-    login_failed = False
-    if request.POST:
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = auth.models.User.objects.create_user(form.cleaned_data['username'],
-                                                        form.cleaned_data['email'],
-                                                        form.cleaned_data['password1'])
-            
-            user = auth.authenticate(username=form.cleaned_data['username'],
-                                     password=form.cleaned_data['password1'])
-            auth.login(request, user)
-            next = form.cleaned_data.get('next') or \
-                   settings.DEVILRY_URLPATH_PREFIX or '/'
-            return http.HttpResponseRedirect(next)
-        elif request.POST.get('registerlink') is not None:
-            print "WAHOOO!"
-            form = RegisterForm(initial={'next': request.GET.get('next')})
-    else:
-        form = RegisterForm(initial={'next': request.GET.get('next')})
-    return render(request,
-                  'authenticate/login.django.html',
-                  {'form': form,
-                   'login_failed': login_failed,
-                   'register': True})
+                   'login_failed': login_failed})
