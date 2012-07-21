91647e87ea33df212900fe974e4c3db0124d0030
Sigmund Hansen <sigmunha@student.uio.no>
2012-07-21 17:20:51 +0200

    Changed SimplifiedExercise to use the fakefieldcleaner decorator.

--- trix/apps/trix/simplified/exercise.py
+++ trix/apps/trix/simplified/exercise.py
@@ -1,7 +1,9 @@
 from trix.apps.trix.models import Exercise, Topic
 from devilry.simplified import FieldSpec, simplified_modelapi, SimplifiedModelApi
 from authorization import AuthorizationMixin
+from fakefieldcleaner import fakefieldcleaner
 
+@fakefieldcleaner
 @simplified_modelapi
 class SimplifiedExercise(AuthorizationMixin):
     """ Simplified wrapper for :class:`trix.apps.trix.models.Exercise`. """
@@ -14,20 +16,8 @@ class SimplifiedExercise(AuthorizationMixin):
 
 #       editablefields = ('id', 'short_name', 'long_name', 'text', 'points')
         fake_editablefields = ('fake_topics', 'fake_prerequisites')
+        fake_real_map =  {'fake_topics': ('topics', Topic, 'pk', 'name')}
+                          #'fake_prerequisites': ('prerequisites', Topic, 'pk', 'name')}
 
         methods = ['create', 'read', 'update', 'delete', 'search']
 
-    @classmethod
-    def post_save(cls, user, obj):
-        if hasattr(obj, 'fake_topics') and isinstance(obj.fake_topics, int):
-            if obj.fake_topics > 0:
-                obj.topics.add(Topic.objects.get(id=obj.fake_topics))
-            else:
-                obj.topics.remove(Topic.objects.get(id=-obj.fake_topics))
-                
-        if hasattr(obj, 'fake_prerequisites') and isinstance(obj.fake_prerequisites, int):
-            if obj.fake_prerequisites > 0:
-                obj.prerequisites.add(Topic.objects.get(id=obj.fake_prerequisites))
-            else:
-                obj.prerequisites.remove(Topic.objects.get(id=-obj.fake_prerequisites))
-        
