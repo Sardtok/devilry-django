313f0f93d723935a35b289cd4ca8229f9e2a9c44
Sigmund Hansen <sigmunha@student.uio.no>
2012-08-07 11:15:12 +0200

    Added followers filter to period group.

--- trix/apps/trix/simplified/periodgroup.py
+++ trix/apps/trix/simplified/periodgroup.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from trix.apps.trix.models import PeriodGroup, Period
-from devilry.simplified import FieldSpec, simplified_modelapi, SimplifiedModelApi
+from devilry.simplified import FieldSpec, simplified_modelapi, SimplifiedModelApi, FilterSpecs, FilterSpec
 from authorization import AuthorizationMixin
 from fakefieldcleaner import fakefieldcleaner
 
@@ -13,5 +14,6 @@ class SimplifiedPeriodGroup(AuthorizationMixin):
         searchfields = FieldSpec('long_name', 'subject__long_name', 'periods__long_name')
         resultfields = FieldSpec('id', 'long_name', 'subject', 'subject__long_name', 'periods__id',
                                  periods=['periods__short_name', 'periods__long_name'])
-        fake_editablefields = ('fake_periods',)
+        fake_editablefields = ('fake_periods', 'fake_followers', 'fake_students')
         fake_real_map = {'fake_periods': ('periods', Period)}
+        filters = FilterSpecs(FilterSpec('followers__id'))
