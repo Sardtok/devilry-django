d9510277fe1a3c9af6a78571b3f64f56d4a884dc
Sigmund Hansen <sigmunha@student.uio.no>
2012-08-07 15:31:13 +0200

    Added Trix specific logout URL.

--- trix/apps/trix/urls.py
+++ trix/apps/trix/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
 
+from devilry.apps.authenticate.views import logout
+
 from restful import trix_manager
 import views
 
 urlpatterns = patterns('devilry.projects.dev.apps.trix',
     url(r'^$', views.main, name='trix'),
+    url(r'^logout/$', logout, name='trix-logout'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^exercise/(?P<exercise>\d+)/status$',
         views.exercisestatus, name='exercisestatus'),
